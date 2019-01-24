a0a60ccb405d192dcf80ce08c25348ccb1a4b532
侯光强 <houguangqiang@shsnc.com>
2019-01-24 15:31:10 +0800

    init

--- src/main/java/com/hgq/security/support/GlobalExceptionHandler.java
+++ src/main/java/com/hgq/security/support/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.hgq.security.support;
 import com.hgq.security.beans.vo.ResultVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -33,13 +35,13 @@ import static org.springframework.http.HttpStatus.*;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleSystemException(Exception ex, WebRequest request) {
         String path = getPath(request);
-        if (logger.isErrorEnabled()) {
-            logger.error("未处理的异常，请检查相关代码，并修复它，PATH=\"" + path + "\"", ex);
+        if (log.isErrorEnabled()) {
+            log.error("未处理的异常，请检查相关代码，并修复它，PATH=\"" + path + "\"", ex);
         }
         ResultVo body = ResultVo.builder()
                 .status(INTERNAL_SERVER_ERROR.value())
@@ -59,6 +61,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                     .build();
         }
         return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ResultVo body = null;
+        if (ex instanceof MethodArgumentTypeMismatchException) {
+            MethodArgumentTypeMismatchException e = (MethodArgumentTypeMismatchException) ex;
+            String path = getPath(request);
+            String requiredType = "";
+            Class<?> type = e.getRequiredType();
+            if (type != null) {
+                requiredType = type.getSimpleName();
+            }
+            String msg = String.format("%s参数类型不匹配, 需要的类型:%s, 实际的值:%s", e.getName(), requiredType, e.getValue());
+            body = ResultVo.builder()
+                    .status(status.value())
+                    .message(msg)
+                    .path(path)
+                    .build();
+        }
+        return handleExceptionInternal(ex, body, headers, status, request);
     }
 
     @Override
@@ -94,7 +117,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     private ResultVo buildBadRequestResult(HttpStatus status, String msg, String path) {
-        logger.warn("参数校验不通过：{}；PATH=\"{}\"", msg, path);
+        log.warn("参数校验不通过：{}；PATH=\"{}\"", msg, path);
         return ResultVo.builder()
                 .status(status.value())
                 .message(msg)
@@ -113,7 +136,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .message(ex.getMessage())
                 .path(path)
                 .build();
-        logger.warn("权限校验不通过：{}, PATH=\"{}\"", ex.getMessage(), path);
+        log.warn("权限校验不通过：{}, PATH=\"{}\"", ex.getMessage(), path);
         return handleExceptionInternal(ex, body, new HttpHeaders(), FORBIDDEN, request);
     }
 

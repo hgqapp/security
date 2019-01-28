313659e371fa33c4cc9bc0e90e7929e0da77ba87
侯光强 <houguangqiang@shsnc.com>
2019-01-28 10:42:51 +0800

    init

---
+++ src/main/resources/init-script/data.sql
@@ -0,0 +1,10 @@
+/*
+SQLyog Ultimate
+MySQL - 5.6.25-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `users` (`user_id`, `create_time`, `email`, `enabled`, `password`, `phone`, `update_time`, `username`) values('1','1548315730636','','1','{noop}123456','','1548315730636','admin');
+insert into `users` (`user_id`, `create_time`, `email`, `enabled`, `password`, `phone`, `update_time`, `username`) values('6','1548384951287','a@qq.com','1','{bcrypt}$2a$10$0zhBxWBG/imbw1dqdhj3a.BUIgGoyyEjGZ.EkS1Viuk5t5df3OxxG','13345667899','1548384951287','admin1');
+insert into `users` (`user_id`, `create_time`, `email`, `enabled`, `password`, `phone`, `update_time`, `username`) values('8','1548399420563','ab@qq.com','1','{bcrypt}$2a$10$SDl/e0GI7fXzRmft94P11OZpAC5NTe3bjDzP7OOKZwe45yxuxM5zG','13345667599','1548399420563','ceshi');
